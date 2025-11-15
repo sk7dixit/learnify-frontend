040b4446a1945dd9b75240f21210e808172068dd
sk7dixit <shashwatdixit33@gmail.com>
2025-11-15 18:22:51 +0530

    Frontend Update:

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    // This block is crucial for fixing module resolution errors in production builds
     rollupOptions: {
-      external: ['react-dropzone'], // Tell Rollup this module is external
+      external: ['react-dropzone'],
     },
   },
 });
